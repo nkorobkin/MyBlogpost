d5d1e9440f5e4121292f3b954608a7ac00629152
Caps1d <ye.smertenko@gmail.com>
2023-03-24 15:52:48 -0700

    HTML and CSS done 90% for whole website

--- Blogpost_Project/index.php
+++ Blogpost_Project/index.php
@@ -5,8 +5,7 @@
     <meta charset="UTF-8">
     <title>My Blog</title>
     <link rel="stylesheet" href="../css/style2.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
 <body>
@@ -14,8 +13,7 @@
         <div class="container">
             <nav class="navbar navbar-expand-lg navbar-light">
                 <a class="navbar-brand" href="#">My Blog</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
@@ -30,23 +28,23 @@
                             <a class="nav-link" href="#">About</a>
                         </li>
                         <?php
-                            // check if the user is logged in and show/hide the profile button accordingly
-                            session_start();
-                            if (isset($_SESSION["user_id"])) {
-                                echo '<li class="nav-item">
+                        // check if the user is logged in and show/hide the profile button accordingly
+                        session_start();
+                        if (isset($_SESSION["user_id"])) {
+                            echo '<li class="nav-item">
                                         <a class="nav-link" href="profile.php">Profile</a>
                                     </li>';
-                            }
+                        }
                         ?>
                     </ul>
                     <div class="my-2 my-lg-0">
                         <?php
-                            // show either login/logout and signup buttons based on whether the user is logged in
-                            if (isset($_SESSION["user_id"])) {
-                                echo '<a href="logout.php" class="btn btn-outline-light mr-2">Logout</a>';
-                            } else {
-                                echo '<a href="#" class="btn btn-outline-light mr-2">Login</a>';
-                            }
+                        // show either login/logout and signup buttons based on whether the user is logged in
+                        if (isset($_SESSION["user_id"])) {
+                            echo '<a href="logout.php" class="btn btn-outline-light mr-2">Logout</a>';
+                        } else {
+                            echo '<a href="#" class="btn btn-outline-light mr-2">Login</a>';
+                        }
                         ?>
                         <a href="#" class="btn btn-primary">Sign up</a>
                     </div>
@@ -88,12 +86,6 @@
         </div>
     </footer>
 </body>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-    crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNVQ8ew"
-    crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-    crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNVQ8ew" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
